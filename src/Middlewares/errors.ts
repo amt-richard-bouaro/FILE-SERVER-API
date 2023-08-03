882c18a198fec4a03677b14fd5943e879e0b0045
rBOUARO-AMALITECH <richard.bouaro@amalitech.org>
2023-08-03 05:48:33 +0000

    Removed err throwing from errors middleware

--- src/Middlewares/errors.ts
+++ src/Middlewares/errors.ts
@@ -3,9 +3,15 @@ import { SERVER_CONFIG, STATUS } from "../config";
 import { z } from "zod";
 // import {}
 export const notFoundError = ( req: Request, res: Response, next: NextFunction) => {
-    const error = new Error(`Not Found: ${req.originalUrl}`);
-    res.status(STATUS.NOT_FOUND)
-    next(error);
+    // const error = new Error(`Not Found: ${req.originalUrl}`);
+    // res.status(STATUS.NOT_FOUND)
+
+      return res.status(STATUS.NOT_FOUND).json({
+            code: "NOT_FOUND",
+            message: "This route you've specified does not exist",
+            type: "error",
+            error: `Not Found: ${req.originalUrl}`
+      })
 }
 
 export const errorHandler = <T extends Error>(err: T, req: Request, res: Response, next: NextFunction) => {
